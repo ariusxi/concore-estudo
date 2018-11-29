04ae986d2526da328c3db7450e99b6fc196c9c0d
Alef Felix <alef.developerweb@gmail.com>
2018-11-29 15:53:36 -0200

    Ajustes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 
 module.exports = {
-    APP_ID: 'playground',
-    APP_KEY: 'angaung1Wahkaoshae4awu1she5Chahw',
-    CONCORE_ADMIN_USER: 'admin@concore.io',
-    CONCORE_ADMIN_PASSWORD: 'concore'
+    APP_ID: '',
+    APP_KEY: '',
+    CONCORE_ADMIN_USER: '',
+    CONCORE_ADMIN_PASSWORD: ''
 };
